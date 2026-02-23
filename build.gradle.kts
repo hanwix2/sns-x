8809673a9b218a9042d0d8412e24dd3da5bd9488
hanwix2 <alexsjh23@gmail.com>
2026-02-23 20:01:58 +0900

    Feature: Redis 를 이용한 세션 스토리지 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.session:spring-session-data-redis")
     implementation("com.fasterxml.jackson.core:jackson-databind")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
